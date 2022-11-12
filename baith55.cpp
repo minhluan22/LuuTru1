289e249f552debd82556e7c49b8b54b20dbce875
minhluan22 <116693294+minhluan22@users.noreply.github.com>
2022-11-12 16:09:35 +0700

    Add files via upload

--- baith55.cpp
+++ baith55.cpp
@@ -7,6 +7,10 @@
 int chuanhoa(char s[50]);
 int xoa(char s[50],int vitrixoa);
 int xoakt(char s[50]);
+int demtu(char s[50]);
+int demkt(char s[50]);
+void ghep(char s[50], char s2[50]);
+
 
 int main()
 { char s[50], s2[50], key;
@@ -35,11 +39,16 @@ int main()
 	getch();
 	break;
 	case 2:
+	fflush(stdin);
+	demtu(s);
+	demkt(s);
 	//goi ham thuc de hien cong viec tuong ung
 	printf("\nBam phim bat ky de tiep tuc!");
 	getch();
 	break;
 	case 3:
+	fflush(stdin);
+	ghep(s,s2);
 	printf("\nBam phim bat ky de tiep tuc!");
 	//goi ham thuc de hien cong viec tuong ung
 	
@@ -102,5 +111,51 @@ int chuanhoa(char s[50])
 	if (s[strlen(s)-1]==' ')
 		xoa(s,strlen(s)-1);
 }
+int demtu(char s[50])
+	{
+		int i,demtu =1 ;
+		strlwr(s);
+		for (i=0;i<strlen(s);i++)
+		{
+			if (s[i]==' ')
+			demtu++;
+		}
+		printf("\n So tu la :%d",demtu);
+	}
+int demkt(char s[50])
+{
+	int demkt=0;
+	char c;
+	printf("\n Nhap mot ky tu : ");
+	scanf("%c",&c);
+	fflush(stdin);
+	strupr(s);
+	for (int i=0;i<strlen(s);i++)
+		{
+		if (s[i]== toupper (c))
+		demkt++;	
+		}
+	printf("\n So Ky tu La :%d",demkt);
+}
+void ghep(char s[50], char s2[50])
+	{
+		int i,j,size,size1 , size2 ;
+		fflush(stdin);
+		printf(" \n Nhap chuoi s2 :");
+		gets(s2);
+		chuanhoa(s2);
+		size1 =strlen(s);
+		size2=strlen(s);
+		size = size1 + size2;
+		j=0;
+		for (i=size1;i< size;i++)
+			{
+			s[i]=  s2[j];
+			j++;
+			}
+			s[i]='\0';
+			printf("\n Ket qua sau khi noi chuoi la :");
+			printf("%s",s);
+	}
 
 
